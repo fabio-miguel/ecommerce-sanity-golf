11656e4b6c5e48277d670ea9fc7f7dd822867e40
Fabio Miguel <fabiomigf@gmail.com>
2024-08-14 11:05:59 +0100

    Updated sanity vars

--- app/lib/sanity/sanity.js
+++ app/lib/sanity/sanity.js
@@ -1,8 +1,8 @@
 import {createClient} from '@sanity/client';
 
-export const projectId = 'i1fhnssa';
-export const dataset = 'production';
-const apiVersion = '2023-03-30';
+export const projectId = process.env.SANITY_PROJECT_ID;
+export const dataset = process.env.SANITY_DATASET;
+const apiVersion = process.env.SANITY_API_VERSION;
 
 export const client = createClient({
   projectId,
